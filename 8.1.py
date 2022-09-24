f165cd20835cf98c5b3eab3d9f47dcddc6cb43df
Харченко <kharchenkomisha2901@gmail.com>
2022-09-24 14:33:16 +0300

    deleted all

--- 8.1.py
+++
@@ -1,5 +0,0 @@
-def kvadrat(h):
-   m = h ** 2
-   return m
-h = int(input())
-print(kvadrat(h))
--- 9.2.py
+++
@@ -1,4 +0,0 @@
-def kolvo(num, a=0):
-    return kolvo(num // 10, a + 1) if num else a
- 
-print(kolvo(int(input())))
