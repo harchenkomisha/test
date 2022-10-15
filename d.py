cea59e76b64dfdb6633c01d974c3708c8355e51a
Харченко <kharchenkomisha2901@gmail.com>
2022-10-15 13:57:00 +0300

    update 4

--- d.py
+++ d.py
@@ -1,9 +1,14 @@
 N = int(input())
 pupils = {}
 MARCS_COUNT = 3 
+output = []
 
 for _  in range(N):
     pupil =  input().split()
     last_name = pupil[0]
     pupils[last_name] = [int(i) for i in pupil[1:]]
-    print(f'{last_name} {(sum(pupils[last_name]) / MARCS_COUNT):.2f}')
+    avg_mark = sum(pupils[last_name]) / MARCS_COUNT
+    output.append('%s %.2f' %(last_name,avg_mark))
+    
+    print('-' * 20)
+    print('\n'.join(output))
